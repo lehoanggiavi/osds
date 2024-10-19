47e6b9f2b6f95e637c5e2e3edc34623ec432130d
giavi-2004 <lehoanggiavi21082004@gmail.com>
2024-10-19 17:45:23 +0700

    Updated all file

--- ScrapData/BaiTap01/Bia_SG.py
+++ ScrapData/BaiTap01/Bia_SG.py
@@ -2,20 +2,99 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
+import sqlite3
 
 
 # ##########################################################
-# Tao 1 DataFrame rong
-d = pd.DataFrame({
-    'Day_Tranception':[],
-    'Open_Price':[],
-    'Highest_Price':[],
-    'Lowest_Price':[],
-    'Close_Price':[],
-    'Change_Price':[],
-    'Change_Percent_Price':[],
-    'Volumn':[]
-    })
+# Ket noi den CSDL
+conn = sqlite3.connect("Stock_Bia_SG.db")
+# Tao doi tuong con tro 
+c = conn.cursor()
+try:
+    c.execute("""
+        Create Table Transacction(
+            Id Integer Primary Key AutoIncrement,
+            Day_Transaction Text,
+            Open_price Integer,
+            Highest_Price Integer,
+            Lowest_Price Integer,
+            Close_Price Integer,
+            Change_Price Text,
+            Change_Percent_Price Text,
+            Volumn Integer
+            )
+    """)
+except Exception as e:
+    print(e)
+# Ngat ket noi
+conn.close()
+
+# Tao ra 1 ham de them du lieu vao CSDL 
+def them(Day_Transaction,
+            Open_price,
+            Highest_Price,
+            Lowest_Price,
+            Close_Price,
+            Change_Price,
+            Change_Percent_Price,
+            Volumn):
+    # Ket noi den CSDL
+    conn = sqlite3.connect('Stock_Bia_SG.db')
+    c = conn.cursor()
+
+    c.execute("""
+    Insert Into Transacction
+        (
+            Day_Transaction,
+            Open_price,
+            Highest_Price,
+            Lowest_Price,
+            Close_Price,
+            Change_Price,
+            Change_Percent_Price,
+            Volumn
+        )
+    Values
+        (
+            :Day_Transaction,
+            :Open_price,
+            :Highest_Price,
+            :Lowest_Price,
+            :Close_Price,
+            :Change_Price,
+            :Change_Percent_Price,
+            :Volumn
+        )
+    """, 
+        {
+            'Day_Transaction': day,
+            'Open_price':open_price,
+            'Highest_Price':highest_price,
+            'Lowest_Price':lowest_price,
+            'Close_Price':close_price,
+            'Change_Price':change_price,
+            'Change_Percent_Price':change_percent_price,
+            'Volumn':volumn
+        })
+    # Chay lai du lieu khi da bi thay doi
+    conn.commit()
+    conn.close()
+    
+
+
+
+# ##########################################################
+# # Tao 1 DataFrame rong
+# d = pd.DataFrame({
+#     'Day_Transaction':[],
+#     'Open_Price':[],
+#     'Highest_Price':[],
+#     'Lowest_Price':[],
+#     'Close_Price':[],
+#     'Change_Price':[],
+#     'Change_Percent_Price':[],
+#     'Volumn':[]
+#     })
 
 # Khoi tao trang web 
 driver = webdriver.Chrome()
@@ -54,92 +133,112 @@ close_prices=[]
 change_prices=[]
 change_percent_prices=[]
 volumns=[]
-for row in rows: 
-    # Lay ngay giao dich
-    try:
-        # day_element = row.find_element(By.XPATH, """//tbody[@class='simplize-table-tbody']
-                                    #    /tr[@class='simplize-table-row simplize-table-row-level-0']/td[1]/h6""")
-        day_element = row.find_element(By.XPATH, "./td[1]/h6")
-        day = day_element.text
-        days.append(day)
-    except Exception as e:
-        print(e)
 
-    # Lay gia mo cua
-    try:
-        open_price_element = row.find_element(By.XPATH, "./td[2]/h6")
-        open_price = open_price_element.text
-        open_prices.append(open_price)
-    except Exception as e:
-        print(e)
+try:
+    for page_num in range(1, 3):
+        page_xpath = f"//ul[@class='simplize-pagination css-10ewz0g']//li[contains(@class, 'simplize-pagination-item') and contains(@class, 'simplize-pagination-item-{page_num}')]/a"
+        page = driver.find_element(By.XPATH, page_xpath)
+        page.click()
+        time.sleep(2)
+        for row in rows: 
+            # Lay ngay giao dich
+            try:
+                # day_element = row.find_element(By.XPATH, """//tbody[@class='simplize-table-tbody']
+                                            #    /tr[@class='simplize-table-row simplize-table-row-level-0']/td[1]/h6""")
+                day_element = row.find_element(By.XPATH, "./td[1]/h6")
+                day = day_element.text
+                days.append(day)
+            except Exception as e:
+                print(e)
 
-    # Lay gia cao nhat
-    try:
-        highest_price_element = row.find_element(By.XPATH, "./td[3]/h6")
-        highest_price = highest_price_element.text
-        highest_prices.append(highest_price)
-    except Exception as e:
-        print(e)
+            # Lay gia mo cua
+            try:
+                open_price_element = row.find_element(By.XPATH, "./td[2]/h6")
+                open_price = open_price_element.text
+                open_prices.append(open_price)
+            except Exception as e:
+                print(e)
 
-    # Lay gia thap nhat
-    try:
-        lowest_price_element = row.find_element(By.XPATH, "./td[4]/h6")
-        lowest_price = highest_price_element.text
-        lowest_prices.append(lowest_price)
-    except Exception as e:
-        print(e)
+            # Lay gia cao nhat
+            try:
+                highest_price_element = row.find_element(By.XPATH, "./td[3]/h6")
+                highest_price = highest_price_element.text
+                highest_prices.append(highest_price)
+            except Exception as e:
+                print(e)
 
-    # Lay gia dong cua
-    try:
-        close_price_element = row.find_element(By.XPATH, "./td[5]/h6")
-        close_price = highest_price_element.text
-        close_prices.append(close_price)
-    except Exception as e:
-        print(e)
+            # Lay gia thap nhat
+            try:
+                lowest_price_element = row.find_element(By.XPATH, "./td[4]/h6")
+                lowest_price = lowest_price_element.text
+                lowest_prices.append(lowest_price)
+            except Exception as e:
+                print(e)
 
-    # Lay thay doi gia
-    try:
-        change_price_element = row.find_element(By.XPATH, "./td[6]/h6")
-        change_price = change_price_element.text
-        change_prices.append(change_price)
-    except Exception as e:
-        print(e)
+            # Lay gia dong cua
+            try:
+                close_price_element = row.find_element(By.XPATH, "./td[5]/h6")
+                close_price = close_price_element.text
+                close_prices.append(close_price)
+            except Exception as e:
+                print(e)
 
-    # Lay % thay doi gia
-    try:
-        # change_percent_price_element = row.find_element(By.XPATH, "./td[7]/div/div/h6")
-        change_percent_price_element = row.find_element(By.XPATH, "./td[7]//h6")
-        change_percent_price = change_percent_price_element.text
-        change_percent_prices.append(change_percent_price)
-    except Exception as e:
-        print(e)
+            # Lay thay doi gia
+            try:
+                change_price_element = row.find_element(By.XPATH, "./td[6]/h6")
+                change_price = change_price_element.text
+                change_prices.append(change_price)
+            except Exception as e:
+                print(e)
 
-    # Lay khoi luong
-    try:
-        volumn_element = row.find_element(By.XPATH, "./td[8]/h6")
-        volumn = volumn_element.text
-        volumns.append(volumn)
-    except Exception as e:
-        print(e)
+            # Lay % thay doi gia
+            try:
+                # change_percent_price_element = row.find_element(By.XPATH, "./td[7]/div/div/h6")
+                change_percent_price_element = row.find_element(By.XPATH, "./td[7]//h6")
+                change_percent_price = change_percent_price_element.text
+                change_percent_prices.append(change_percent_price)
+            except Exception as e:
+                print(e)
 
-    # Tao Dictionary cho Bia_SG
-    Stock_Bia_SG = {
-        'Day_Tranception': day,
-        'Open_Price':open_price,
-        'Highest_Price':highest_price,
-        'Lowest_Price':lowest_price,
-        'Close_Price':close_price,
-        'Change_Price':change_price,
-        'Change_Percent_Price':change_percent_price,
-        'Volumn':volumn
-        }
+            # Lay khoi luong
+            try:
+                volumn_element = row.find_element(By.XPATH, "./td[8]/h6")
+                volumn = volumn_element.text
+                volumns.append(volumn)
+            except Exception as e:
+                print(e)
+            
+            them(
+                day,
+                open_price,
+                highest_price,
+                lowest_price,
+                close_price,
+                change_price,
+                change_percent_price,
+                volumn
+            )
+            # # Tao Dictionary cho Bia_SG
+            # Stock_Bia_SG = {
+            #     'Day_Transaction': day,
+            #     'Open_Price':open_price,
+            #     'Highest_Price':highest_price,
+            #     'Lowest_Price':lowest_price,
+            #     'Close_Price':close_price,
+            #     'Change_Price':change_price,
+            #     'Change_Percent_Price':change_percent_price,
+            #     'Volumn':volumn
+            #     }
 
-    # Chuyen doi dictionary thanh DF
-    Stock_Bia_SG_df = pd.DataFrame([Stock_Bia_SG])
+            # # Chuyen doi dictionary thanh DF
+            # Stock_Bia_SG_df = pd.DataFrame([Stock_Bia_SG])
 
-    # Them thong tin vao DF chinh
-    d = pd.concat([d, Stock_Bia_SG_df], ignore_index=True)
-
+            # # Them thong tin vao DF chinh
+            # d = pd.concat([d, Stock_Bia_SG_df], ignore_index=True)
+            
+            
+except Exception as e:
+    print(e)
 
 
 # print("Ngay mo giao dich")
@@ -175,14 +274,14 @@ for row in rows:
 # print("##############################")
 
 
-print(d)
+# print(d)
 
-# determining the name of the file
-file_name = 'Stock_Bia_SG.xlsx'
+# # determining the name of the file
+# file_name = 'Stock_Bia_SG.xlsx'
 
-# saving the excel
-d.to_excel(file_name)
-print('DataFrame is written to Excel File successfully.')
+# # saving the excel
+# d.to_excel(file_name)
+# print('DataFrame is written to Excel File successfully.')
 driver.quit()
 
 
